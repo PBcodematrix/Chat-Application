29c6eaa00da876a8745ce8a38e9e2c8fbd9df1cf
PBcodematrix <119732783+PBcodematrix@users.noreply.github.com>
2024-01-06 11:18:25 +0530

    new

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express=require("express");
-const path=require("path");
 const morgan=require("morgan");
 
 const rateLimit=require("express-rate-limit");
@@ -34,13 +33,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(helmet());
 
-if (process.env.NODE_ENV === "production") {
-    //set static folder
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-  }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 
 const limiter=rateLimit({
     max:3000,
